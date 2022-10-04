06e325d27c6cd6b6a57abd8e9fa48e8366e25388
Seth Pennebaker <fusecreated@gmail.com>
2022-10-04 11:54:00 -0400

    adding files

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,20 +4,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { NgApexchartsModule } from 'ng-apexcharts';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+
 import { PaymentsPageComponent } from './payments-page/payments-page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-=======
+
 
 import { FormsModule } from '@angular/forms';
 import { LoanCalculatorComponent } from './loan-calculator/loan-calculator.component';
 
->>>>>>> 90796c6bd72907d1e8a56da3ffcd89296510423d
+
 import { CreateAccountComponent } from './create-account/create-account.component';
 
 
 @NgModule({
-<<<<<<< HEAD
   declarations: [
     AppComponent,
     PaymentsPageComponent,
@@ -29,10 +28,8 @@ import { CreateAccountComponent } from './create-account/create-account.componen
     BrowserModule,
     AppRoutingModule
   ],
-=======
   declarations: [AppComponent, LoanCalculatorComponent, CreateAccountComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, NgApexchartsModule],
->>>>>>> 90796c6bd72907d1e8a56da3ffcd89296510423d
   providers: [],
   bootstrap: [AppComponent],
 })
