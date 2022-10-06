1ca510277abd87a23ec91bac4d7e7ce7df33d962
Anthony W <45855928+Ant626@users.noreply.github.com>
2022-10-06 09:52:34 -0400

    pushing my files

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home-page/home.component';
+import { CreateBudgetComponent } from './create-budget/create-budget.component';
+import { ViewBudgetComponent } from './view-budget/view-budget.component';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+                          
+                          {path: '', component: HomeComponent},
+                          {path: 'create-budget', component: CreateBudgetComponent},
+                          {path: 'view-budget', component: ViewBudgetComponent},
+                          
+                      ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home-page/home.component';
+import { CreateBudgetComponent } from './create-budget/create-budget.component';
+import { ViewBudgetComponent } from './view-budget/view-budget.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UserLoginComponent } from './user-login/user-login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CreateBudgetComponent,
+    ViewBudgetComponent,
+    UserLoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgApexchartsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
